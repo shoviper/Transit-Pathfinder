a7464bf333e64d5de43ef7f3c245f7c0788b31f0
Intutch Sribanyaranond <65011306@kmitl.ac.th>
2024-11-16 13:06:20 +0700

    Update ืnewtrain.pl

--- View/ืnewtrain.pl
+++ View/ืnewtrain.pl
@@ -3,13 +3,82 @@
     =========================================
     
     % Estimate Cost
-    cost(no. Of stations, cost)
+    cost(no. Of stations, cost, line).
 
     =========================================
 */
 % BTS dark green line
+cost(0, 17, dark_green).
+cost(1, 17, dark_green).
+cost(2, 25, dark_green).
+cost(3, 28, dark_green).
+cost(4, 32, dark_green).
+cost(5, 35, dark_green).
+cost(6, 40, dark_green).
+cost(7, 43, dark_green).
+cost(8, 47, dark_green).
+cost(9, 47, dark_green).
+cost(10, 47, dark_green).
+cost(11, 47, dark_green).
+cost(12, 47, dark_green).
+cost(13, 47, dark_green).
+cost(14, 47, dark_green).
+
 % BTS light green line
+cost(0, 17, light_green).
+cost(1, 17, light_green).
+cost(2, 25, light_green).
+cost(3, 28, light_green).
+cost(4, 32, light_green).
+cost(5, 35, light_green).
+cost(6, 40, light_green).
+cost(7, 43, light_green).
+cost(8, 47, light_green).
+cost(9, 47, light_green).
+cost(10, 47, light_green).
+cost(11, 47, light_green).
+cost(12, 47, light_green).
+cost(13, 47, light_green).
+cost(14, 47, light_green).
+cost(15, 47, light_green).
+cost(16, 47, light_green).
+cost(17, 47, light_green).
+cost(18, 47, light_green).
+cost(19, 47, light_green).
+cost(20, 47, light_green).
+cost(21, 47, light_green).
+cost(22, 47, light_green).
+cost(23, 47, light_green).
+cost(24, 47, light_green).
+cost(25, 47, light_green).
+cost(26, 47, light_green).
+cost(27, 47, light_green).
+cost(28, 47, light_green).
+cost(29, 47, light_green).
+cost(30, 47, light_green).
+cost(31, 47, light_green).
+cost(32, 47, light_green).
+cost(33, 47, light_green).
+cost(34, 47, light_green).
+cost(35, 47, light_green).
+cost(36, 47, light_green).
+cost(37, 47, light_green).
+cost(38, 47, light_green).
+cost(39, 47, light_green).
+cost(40, 47, light_green).
+cost(41, 47, light_green).
+cost(42, 47, light_green).
+cost(43, 47, light_green).
+cost(44, 47, light_green).
+cost(45, 47, light_green).
+cost(46, 47, light_green).
+cost(47, 47, light_green).
+cost(48, 47, light_green).
+
 % BTS gold line
+cost(0, 16, gold).
+cost(1, 16, gold).
+cost(2, 16, gold).
 
 % MRT pink line
 % MRT yellow line
