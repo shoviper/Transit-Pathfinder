93feb56a18d8d5208e42f72f933b33082d848911
shoviper <65011304@kmitl.ac.th>
2024-11-16 12:49:34 +0700

    fix the fact of estimated cost

---
+++ View/ืnewtrain.pl
@@ -0,0 +1,21 @@
+% Estimate Cost
+/*
+    =========================================
+    
+    % Estimate Cost
+    cost(no. Of stations, cost)
+
+    =========================================
+*/
+% BTS dark green line
+% BTS light green line
+% BTS gold line
+
+% MRT pink line
+% MRT yellow line
+% MRT blue line
+% MRT purple line
+
+% SRTET red line
+
+% APL line
